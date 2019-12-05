3d0a204a3968f6a8ffc0af1295d05714b07ab746
ardroid <arturporohow@gmail.com>
2019-12-05 23:13:43 +0200

    Test commit

---
+++ app/src/main/java/com/ardroid/petroom/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ardroid.petroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //test
+    }
+}
