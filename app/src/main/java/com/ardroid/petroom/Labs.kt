6082da41003e811255e0d8fd131c818e012f3872
admin <arturporohow@gmail.com>
2020-03-04 21:25:40 +0300

    Lab1 Done

--- app/src/main/java/com/ardroid/petroom/Labs.kt
+++ app/src/main/java/com/ardroid/petroom/Labs.kt
@@ -29,20 +29,22 @@ class Labs {
 
     fun doAllLabs(): List<File?> {
         return listOf(
-                lab1()
+                lab1(),
+                lab2(),
+                lab18()
+
         )
     }
 
 
-
     fun lab1(): File? {
-        val inputFile = File(externalFile.path + "/" + "input1.txt")
+        val inputFile = File(externalFile.path + "/" + "input16.txt")
         val scanner = Scanner(inputFile)
         val stringBuilder = StringBuilder()
-        while (scanner.hasNextInt()){
+        while (scanner.hasNextInt()) {
             var sum = 0
             val size = scanner.nextInt()
-            for (i in 0 until size){
+            for (i in 0 until size) {
                 sum += scanner.nextInt()
             }
             stringBuilder.appendln(sum)
@@ -50,4 +52,46 @@ class Labs {
         return writeFileOnExternalStorage(1, stringBuilder.toString())
     }
 
+    fun lab2(): File? {
+        val inputFile = File(externalFile.path + "/" + "input17.txt")
+        val scanner = Scanner(inputFile)
+        val stringBuilder = StringBuilder()
+        while (scanner.hasNextInt()) {
+            val evenList = mutableListOf<Int>()
+            val oddList = mutableListOf<Int>()
+            val size = scanner.nextInt()
+            for (i in 0 until size) {
+                val number = scanner.nextInt()
+                if (number % 2 == 0) evenList.add(number)
+                else oddList.add(number)
+            }
+            stringBuilder.appendln(evenList.joinToString(" ") +
+                    " " + oddList.joinToString(" "))
+        }
+
+        return writeFileOnExternalStorage(17, stringBuilder.toString())
+    }
+
+    fun lab18(): File? {
+        val inputFile = File(externalFile.path + "/" + "input18.txt")
+        val scanner = Scanner(inputFile)
+        val stringBuilder = StringBuilder()
+        while (scanner.hasNextInt()) {
+            val list = mutableListOf<Int>()
+            val size = scanner.nextInt()
+            for (i in 0 until size) {
+                list.add(scanner.nextInt())
+            }
+            for (i in 0 until size/2 ) {
+                val temp = list[i]
+                list[i] = list[size - i - 1]
+                list[size - i - 1] = temp
+            }
+            stringBuilder.appendln(list.joinToString(" "))
+        }
+
+        return writeFileOnExternalStorage(18, stringBuilder.toString())
+    }
+
 }
+
